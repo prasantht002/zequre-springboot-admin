8bfdc08a2b7199ff6207684f2be1c1460de299a6
prasantht002 <128283786+prasantht002@users.noreply.github.com>
2025-12-22 15:43:58 +0530

    Add files via upload

---
+++ zequre-springboot-admin/src/main/java/com/example/zequre_springboot_admin/ZequreSpringbootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.zequre_springboot_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZequreSpringbootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZequreSpringbootAdminApplication.class, args);
+	}
+
+}
